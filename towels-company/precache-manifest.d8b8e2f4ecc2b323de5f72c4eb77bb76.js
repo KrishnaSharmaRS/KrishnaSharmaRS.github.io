a2a68fe122ae5cfe73a5e7e64e600a62599a08c9
Krishna Sharma <Krrish.rbs@gmail.com>
2020-10-16 14:37:04 +0530

    Added the Towels Project

---
+++ towels-company/precache-manifest.d8b8e2f4ecc2b323de5f72c4eb77bb76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb53c280cf82a99385169a9b17e92a56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8443ec5fed65b9fc0d50",
+    "url": "/static/css/main.0bbf63cc.chunk.css"
+  },
+  {
+    "revision": "dd30083985b977f9301e",
+    "url": "/static/js/2.4791faa4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4791faa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8443ec5fed65b9fc0d50",
+    "url": "/static/js/main.ecf0eee8.chunk.js"
+  },
+  {
+    "revision": "1e177cb95b12153b715f",
+    "url": "/static/js/runtime-main.e202249f.js"
+  }
+]);
